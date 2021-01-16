2aab9fcf5b0f452f85ce1c8ee717b0497a7f329f
YulDmi <barsetca@rambler.ru>
2021-01-16 15:24:07 +0300

    pattern

--- eureka-client/src/main/java/com/geekbrains/cloud/ProductService.java
+++ eureka-client/src/main/java/com/geekbrains/cloud/ProductService.java
@@ -4,19 +4,53 @@ package com.geekbrains.cloud;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @Service
 @AllArgsConstructor
 public class ProductService {
+
+    private static Map<Long, Product> map = new HashMap<>();
     private ProductRepository productRepository;
 
-    public List<Product> findAll(){
-        return productRepository.findAll();
+    public List<Product> findAll() {
+        long count = productRepository.count();
+        if ((long)map.size() != count){
+            List<Product> products = productRepository.findAll();
+           products.forEach(product -> map.put(product.getId(), product));
+            return products;
+        }else {
+            return new ArrayList<>(map.values());
+        }
+    }
+
+    public Product save(Product product) {
+
+        Product p = productRepository.save(product);
+        map.put(p.getId(), p);
+        return p;
+    }
+
+    public void delete(Long id) {
+        productRepository.deleteById(id);
+        map.remove(id);
 
     }
-    public Product save(Product product){
-      return  productRepository.save(product);
+
+    public Product getProduct(Long id) {
+        if(map.containsKey(id)){
+            return map.get(id);
+        }
+        Product p = productRepository.findById(id).orElse(null);
+        if(p != null){
+            map.put(id, p);
+        }
+        return p;
     }
+
 }
